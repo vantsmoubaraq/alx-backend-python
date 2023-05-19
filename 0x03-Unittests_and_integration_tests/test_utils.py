db56b826d1088b92098d2c69d24cf5ca6ff52b8d
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-19 15:07:50 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -30,7 +30,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand(
         [
             ({}, ("a",)),
-            ({"a": 1}, ("a", "b"))
+            ({"a": 1}, ("a", "b")),
         ]
     )
     def test_access_nested_map_exception(self, nested_map: Dict,
