5648e6f825802a2536d8c7367c7d840bfb7690cf
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-19 14:58:43 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,7 @@ Module implements unit test
 import unittest
 from parameterized import parameterized, parameterized_class
 import requests
-from typing import Dict, Sequence, Any, Union, Tuple
+from typing import Dict, Union, Tuple
 access_nested_map = __import__("utils").access_nested_map
 get_json = __import__("utils").get_json
 
@@ -23,6 +23,39 @@ class TestAccessNestedMap(unittest.TestCase):
         ]
     )
     def test_access_nested_map(self, nested_map: Dict,
-                               path: Tuple, expected: Union[int, str]):
+                               path: Tuple, expected: int):
         """Tests access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple, expected: str):
+        """Tests raise of keyError exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class tests utils.get_json"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @unittest.mock.patch("requests.get")
+    def test_get_json(self, test_url: str, expected: Dict,
+                      mock_get: unittest.mock.Mock):
+        """tests get_json"""
+
+        mock_get.return_value.json.return_value = expected
+        result = get_json(test_url)
+
+        self.assertEqual(result, expected)
+        mock_get.assert_called_once_with(test_url)
