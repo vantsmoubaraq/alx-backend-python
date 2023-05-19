1aece3954c042b5cfda903794b480b42bca550ba
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-19 14:49:38 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,34 +26,3 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Sequence, expected: Any):
         """Tests access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand(
-        [
-            ({}, ("a",), KeyError),
-            ({"a": 1}, ("a", "b"), KeyError)
-        ]
-    )
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        """Tests raise of keyError exception"""
-        with self.assertRaises(expected):
-            access_nested_map(nested_map, path)
-
-
-class TestGetJson(unittest.TestCase):
-    """Class tests utils.get_json"""
-
-    @parameterized.expand(
-        [
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False}),
-        ]
-    )
-    @unittest.mock.patch("requests.get")
-    def test_get_json(self, test_url, expected, mock_get):
-        """tests get_json"""
-
-        mock_get.return_value.json.return_value = expected
-        result = get_json(test_url)
-
-        self.assertEqual(result, expected)
-        mock_get.assert_called_once_with(test_url)
