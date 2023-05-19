a02faf06697df8f73ee5ea68f7347a3c56c7094f
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-19 15:51:43 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -56,6 +56,5 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.return_value.json.return_value = expected
         result = get_json(test_url)
-
         self.assertEqual(result, expected)
         mock_get.assert_called_once_with(test_url)
