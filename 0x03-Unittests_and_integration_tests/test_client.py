63e5289e065264459fcc5ea4bdf2381189acd57f
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-20 21:56:09 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,3 +38,15 @@ class TestGithubOrgClient(TestCase):
                     return_value=payload)):
             obj = GithubOrgClient("TechAccess")
             self.assertEqual(obj._public_repos_url, expected)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_obj: Mock):
+        """Method tests property _public_repos"""
+        expected = {"Command": "git_push"}
+        mock_obj.return_value = expected
+
+        result = "github.com/techaccess"
+        with patch("client.GithubOrgClient._public_repos_url", PropertyMock(
+                    return_value=result)) as mock_repo:
+            obj = GithubOrgClient("TechAccess")
+            self.assertEqual(obj._public_repos_url, result)
