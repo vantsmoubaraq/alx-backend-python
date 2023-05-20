fb0f6d6cd2881f4c864bfead2cb48765e960998d
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-20 09:01:48 +0300

    Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 Module implements unit test
 """
 
-import unittest
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
 from parameterized import parameterized, parameterized_class
 import requests
 from typing import Dict, Union, Tuple
@@ -13,7 +14,7 @@ get_json = __import__("utils").get_json
 memoize = __import__("utils").memoize
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """class implements unittest for TestAccessNestedMap"""
 
     @parameterized.expand(
@@ -41,9 +42,8 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
-"""
-class TestGetJson(unittest.TestCase):
-    ""Class tests utils.get_json""
+class TestGetJson(TestCase):
+    """Class tests utils.get_json"""
 
     @parameterized.expand(
         [
@@ -51,19 +51,18 @@ class TestGetJson(unittest.TestCase):
             ("http://holberton.io", {"payload": False}),
         ]
     )
-    @unittest.mock.patch("requests.get")
-    def test_get_json(self, test_url: str, expected: Dict,
-                      mock_get: unittest.mock.Mock):
-        ""tests get_json""
+    def test_get_json(self, test_url: str, expected: Dict):
+        """tests get_json"""
+        mock_get = Mock()
+        mock_get.json.return_value = expected
 
-        mock_get.return_value.json.return_value = expected
-        result = get_json(test_url)
-        self.assertEqual(result, expected)
-        mock_get.assert_called_once_with(test_url)
-"""
+        with patch("requests.get", return_value=mock_get):
+            result = get_json(test_url)
+            self.assertEqual(result, expected)
+            mock_get.assert_called_once()
 
 
-class TestMemoize(unittest.TestCase):
+class TestMemoize(TestCase):
     """Class tests memoize function"""
 
     def test_memoize(self):
@@ -75,9 +74,9 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-        
+
         test_class = TestClass()
-        with unittest.mock.patch.object(TestClass, "a_method") as mock_method:
+        with patch.object(TestClass, "a_method") as mock_method:
             self.assertEqual(test_class.a_property, 42)
             self.assertEqual(test_class.a_property, 42)
             mock_method.assert_called_once()
