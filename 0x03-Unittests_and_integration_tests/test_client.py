7fdee981dc744cefdf63278171e8c40905fb7824
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-20 14:57:49 +0300

    Unittests_and_integration_tests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Module tests GithubOrgClient methods
+"""
+
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
+from typing import Dict
+from client import (
+    GithubOrgClient,
+)
+from parameterized import parameterized
+from utils import (
+    get_json,
+    access_nested_map,
+    memoize,
+)
+
+
+class TestGithubOrgClient(TestCase):
+    """
+    Class tests GithubOrgClient methods
+    """
+
+    @parameterized.expand(
+        [
+            ("google", {"google": True}),
+            ("abc", {"abc": True}),
+        ]
+    )
+    @patch("get_json")
+    def test_org(self, org, expected: Dict, mock_org: Mock):
+        """Tests org method"""
+        mock_org.return_value = expected
+        obj = GithubOrgClient(org)
+        self.assertEqual(obj.org, expected)
+        mock_org.assert_called_once_with("https://api.github.com/orgs/" + org)
